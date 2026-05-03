deb7782f445c56001253b445ff2cb271ffdccf9a
ASOCIACION ENFERMERIA <reservaurbanaok@gmail.com>
2026-05-03 01:17:29 -0300

    Add files via upload

--- dashboard/config.js
+++ dashboard/config.js
@@ -1,4 +1,4 @@
 window.CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyifzgSMhYas79GAYRHCgLZS6i7zZKpLMom-e8FPfbfZu_y2l-xOdfoe6slskcv1v5WWA/exec",
-  GA4_URL: "https://script.google.com/macros/s/AKfycbwoct1vBL9jcgmz-C4O4D8Dj6JLpDyAgKFdeklIO_qmIVJYf6TwTOJRsS5Acnc3Cuf9XQ/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzSzB061txoSMo4PuTk7h217QeNemGDkrP_WPP9o09ot0oUhGZxLMht_Yrp0IkHFa7l3w/exec",
+  GA4_URL: "https://script.google.com/macros/s/AKfycbzSzB061txoSMo4PuTk7h217QeNemGDkrP_WPP9o09ot0oUhGZxLMht_Yrp0IkHFa7l3w/exec?tipo=ga4"
 };
